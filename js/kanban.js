6e1854e2191116553fbddf33e52a13b0df35d895
Wendy Afrim <wendy.afrim2@gmail.com>
2022-06-14 23:49:45 +0200

    Modifiy kanban styling Back logic to add& get a new ticket in the local storage

--- js/kanban.js
+++ js/kanban.js
@@ -1,11 +1,75 @@
-import pattern from "./routes";
-class Api
+export default class Api
 {
+    static getTickets(ticket_id) {
+        let tickets = readSession();
+
+        let existing_tickets = tickets.find(ticket => ticket.id === ticket_id);
+
+        if (!existing_tickets) {
+            return [];
+        }
+        return existing_tickets;
+    }
+
+    static insertTicket(column_id, content) {
+        let columns = readSession();
+        let column = columns.find(column => column.id === column_id);
+
+        let ticket = {
+            id : Math.floor(Math.random() * 100000),
+            content : content
+        }
+
+        if (!column) {
+            throw new Error('Column not found');
+        }
+
+        column.tickets.push(ticket);
+        saveInSession(columns);
+
+        return ticket;
+    }
+
+    static updateTicket(ticket_id, update){
+        let columns = readSession();
+    }
+
 
 }
 
-let addtask = document.getElementById("add-task");
+function readSession()
+{
+    let data = localStorage.getItem('kanban_data');
 
-addtask.addEventListener("click", () => {
-    console.log("click");
-});
+    if (!data) {
+        return [
+            {
+                id : 1,
+                status : 'to_do',
+                tickets : []
+            },
+            {
+                id : 2,
+                status : 'in_progress',
+                tickets : []
+            },
+            {
+                id : 3,
+                status: 'to_come',
+                tickets : []
+            },
+            {
+                id : 3,
+                status: 'done',
+                tickets : []
+            },
+        ]
+    }
+
+    return JSON.parse(data);
+}
+
+function saveInSession(data)
+{
+    localStorage.setItem('kanban_data', JSON.stringify(data));
+}
